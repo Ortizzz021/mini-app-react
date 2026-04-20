9c2d69f6dde50fc19d833c70245c2b8590e1854a
Ortizzz021 <dortizortiz021@gmail.com>
2026-04-20 16:51:15 -0500

    Reestructuracion 1

--- mini-app/components/Footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="border-t mt-6 py-4 text-center text-sm text-slate-500">
-      <p>© 2026 - Proyecto React</p>
-    </footer>
-  );
-}
